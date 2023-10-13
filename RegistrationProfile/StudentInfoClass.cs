b08e494da7a2f7d5aff766a803a831bd7d313235
remoroza.254068 <remoroza.254068@LAB-A-PC04>
2023-10-13 18:29:18 +0800

    Add project files.

---
+++ RegistrationProfile/StudentInfoClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationProfile
+{
+    internal class StudentInfoClass
+    {
+       public static int StudentNo = 0;
+       public static int ContactNo = 0;
+
+       public static string Program = "";
+       public static string Gender = "";
+       public static string Birthday = "";
+       public static string FullName = "";
+
+       
+    }
+}
